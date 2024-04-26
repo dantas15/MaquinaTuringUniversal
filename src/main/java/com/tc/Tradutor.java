39f57ee71a05e3ae0988f28f7170069cf657df70
Lian <elianfernandosc@gmail.com>
2024-04-26 15:03:48 -0300

    Encode (utilizando switch para melhor manutenção de código)

--- src/main/java/com/tc/Tradutor.java
+++ src/main/java/com/tc/Tradutor.java
@@ -7,24 +7,25 @@ public class Tradutor{
     public Tradutor(){
 
     }
-    public String en(String valor){//enconde
-        if(valor.equals("B")){
-            return "111";
-        } else if (valor.equals("0")){
-            return "1";
-        } else if (valor.equals("1")){
-            return "11";
-        } else if (valor.equals("L")){
-            return "1";
-        } else if (valor.equals("R")){
-            return "11";
-        } else {
-            String x = "111";
-            int y = Integer.parseInt(valor);
-            for(int contador = 2; contador < y; contador++){
-                x = x + "1";
-            }
-            return x;
+    public String en(String valor){//encode
+        switch(valor){
+            case "B":
+                return "111";
+            case "0":
+                return "1";
+            case "1":
+                return "11";
+            case "L":
+                return "1";
+            case "R":
+                return "11";
+            default:
+                String x = "111";
+                int y = Integer.parseInt(valor);
+                for(int contador = 2; contador < y; contador++){
+                    x = x + "1";
+                }
+                return x;
         }
     }
 }
